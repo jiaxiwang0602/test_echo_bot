589fd0c13ae9d9c38b115a086cdf582afa0ce71f
jiaxiwang0602 <jiaxi0602@gmail.com>
2025-06-06 16:07:59 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+""" Bot Configuration """
+
+
+class DefaultConfig:
+    """ Bot Configuration """
+
+    PORT = 3978
+    APP_ID = os.environ.get("MicrosoftAppId", "e3094a3a-720e-4d08-adfc-ab4bfd974afd")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_TYPE = os.environ.get("MicrosoftAppType", "UserAssignedMSI")
+    APP_TENANTID = os.environ.get("MicrosoftAppTenantId", "72f988bf-86f1-41af-91ab-2d7cd011db47")
